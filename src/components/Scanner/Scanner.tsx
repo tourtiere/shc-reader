9283d67d5405018773275388aac61eca81420026
tourtiere <leonard-dufour@hotmail.com>
2021-06-02 23:59:18 -0400

    fix some weird stuff one mobile devices. Should be better now.

--- src/components/Scanner/Scanner.tsx
+++ src/components/Scanner/Scanner.tsx
@@ -2,6 +2,9 @@ import { h } from "preact";
 import { useContext, useEffect, useRef, useState } from "preact/hooks";
 import { ScanContext } from "providers/scan";
 import QrScanner from "qr-scanner"; // if installed via package and bundling with a module bundler like webpack or rollup
+
+import imageCompression from "browser-image-compression";
+import "static/loading.gif";
 import "./Scanner.scss";
 import { Buffer } from "buffer";
 import SHC from "types/SHC";
@@ -22,31 +25,34 @@ const extractCode = (qr: string) => {
 
 interface Props {}
 export default (props: Props) => {
-    //const scannerRef = useRef<HTMLVideoElement>(null);
+    const scannerRef = useRef<HTMLVideoElement>(null);
     const fileInputRef = useRef<HTMLInputElement>(null);
     const { setPayload } = useContext(ScanContext);
     const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        // Maybe later:
-        /*
+        // Maybe for later:
 
-        const scanner = new QrScanner(
-            scannerRef.current,
-            (result) => {
-                try {
-                    const shc = extractCode(result);
-                    if (shc) setPayload(shc);
-                    //console.log(shc);
-                } catch {
-                    //console.log("error");
-                }
-            },
-            undefined,
-            undefined,
-            "environment"
-        );
-        scanner.start();
+        /*
+        try {
+            const scanner = new QrScanner(
+                scannerRef.current,
+                (result) => {
+                    try {
+                        const shc = extractCode(result);
+                        if (shc) setPayload(shc);
+                        //console.log(shc);
+                    } catch {
+                        //console.log("error");
+                    }
+                },
+                undefined,
+                undefined,
+                "environment"
+            );
+            scanner.start();
+        } catch {}
         */
 
         fileInputRef.current.addEventListener(
@@ -54,29 +60,35 @@ export default (props: Props) => {
             (onchange = (e: Event) => {
                 const files = fileInputRef.current.files;
                 if (files === null) return;
-                const file = files.item(0);
-                if (file === null) return;
+                const heavyFile = files.item(0);
+                if (heavyFile === null) return;
+                setIsLoading(true);
 
-                //return;
-                QrScanner.scanImage(file, {})
+                imageCompression(heavyFile, { maxSizeMB: 0.1 })
+                    .then((file) =>
+                        QrScanner.scanImage(file, {}, undefined, undefined)
+                            .then((result) => {
+                                const shc = extractCode(result);
+                                if (shc) setPayload(shc);
+                            })
+                            .catch((reason) => {
+                                debugger;
 
-                    .then((result) => {
-                        const shc = extractCode(result);
-                        if (shc) setPayload(shc);
-                    })
-                    .catch((reason) => setError(reason));
+                                setError(reason);
+                            })
+                    )
+                    .finally(() => setIsLoading(false));
             })
         );
     }, []);
 
     return (
         <div className={"scanner-wrapper"}>
-            <p>{error}</p>
             <div>
                 <input type="file" ref={fileInputRef} />
             </div>
+            <p style={{ color: "red" }}>{error}</p>
+            <p>{isLoading ? <img style={{ width: 50, height: 50 }} src="/loading.gif" /> : ""}</p>
         </div>
     );
-
-    //<video ref={scannerRef} />
 };
